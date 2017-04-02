7c2608fcb82c2559a61f6806ee41d05077bddd2f
Gaurav Baruah <gbaruah@uwaterloo.ca>
2017-04-02 15:12:47 -0400

    bridge updated with new model file name convection

--- castorini_smmodel_bridge.py
+++ castorini_smmodel_bridge.py
@@ -148,7 +148,7 @@ class SMModelBridge(object):
 
 if __name__ == "__main__":
 
-    smmodel = SMModelBridge('../models/sm_model/sm.model.aquaint.train-all.2017-04-02',
+    smmodel = SMModelBridge('../models/sm_model/sm_model.TrecQA.TRAIN-ALL.2017-04-02.castor',
                             '../data/word2vec/aquaint+wiki.txt.gz.ndim=50.cache',
                             '../data/TrecQA/stopwords.txt',
                             '../data/TrecQA/word2dfs.p')
