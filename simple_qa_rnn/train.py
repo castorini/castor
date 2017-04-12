85d0b56d8b62fb284df29dad57b50a5ce8865cff
Salman Mohammed <salman93@live.com>
2017-04-11 22:12:52 -0400

    tran acc added

--- simple_qa_rnn/train.py
+++ simple_qa_rnn/train.py
@@ -30,6 +30,11 @@ def evaluate_dataset_batch(data_set, max_sent_length, model, w2v_map, label_to_i
         scores = model(inputs)
         pred_label_ix = np.argmax(scores.data.numpy(), axis=1) # check this properly
         correct_label_ix = targets.data.numpy()
+
+        print("scores size: {}".format(scores.size()))
+        print("pred_label_ix shape: {}".format(pred_label_ix.shape))
+
+
         n_correct += (pred_label_ix == correct_label_ix).sum()
     acc = n_correct / n_total
     return acc
@@ -70,10 +75,10 @@ optimizer = optim.Adam(model.parameters(), lr=args.lr)
 start = time.time()
 best_val_acc = -1
 iter = 0
-header = '  Time Epoch Iteration     Loss  Val/Accuracy'
+header = '  Time Epoch Iteration     Loss  Train/Accuracy   Val/Accuracy'
 print(header)
 log_template = ' '.join('{:>6.0f},{:>5.0f},{:>9.0f},{:>9.6f}'.split(','))
-dev_log_template = ' '.join('{:>6.0f},{:>5.0f},{:>9.0f},{:>9.6f},{:9.6f}'.split(','))
+dev_log_template = ' '.join('{:>6.0f},{:>5.0f},{:>9.0f},{:>9.6f},{:9.6f},{:9.6f}'.split(','))
 
 model.train()
 for epoch in range(args.epochs):
@@ -105,8 +110,9 @@ for epoch in range(args.epochs):
 
         # log at intervals
         if iter % args.dev_every == 0:
+            train_acc = evaluate_dataset_batch(train_set, max_sent_length, model, w2v_map, label_to_ix)
             val_acc = evaluate_dataset_batch(val_set, max_sent_length, model, w2v_map, label_to_ix)
-            print(dev_log_template.format(time.time()-start, epoch, iter, loss.data[0], val_acc))
+            print(dev_log_template.format(time.time()-start, epoch, iter, loss.data[0], train_acc, val_acc))
             if val_acc > best_val_acc:
                 best_val_acc = val_acc
                 snapshot_prefix = os.path.join(args.save_path, 'best_snapshot')
