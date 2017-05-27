4ab7288c12835ed600bd7d3c464b1a52256f3a57
Salman Mohammed <smohammed1993@gmail.com>
2017-05-27 13:09:38 -0400

    set parameters for tuning

--- simple_qa_rnn/hyperparameter_tuning.py
+++ simple_qa_rnn/hyperparameter_tuning.py
@@ -4,13 +4,13 @@ from subprocess import call
 epochs = 20
 count = 500
 for id in range(count):
-    learning_rate = 10 ** uniform(-8, -3)
+    learning_rate = 10 ** uniform(-7, -3)
     d_hidden = randint(300, 800)
-    n_layers = randint(1, 5)
-    dropout = uniform(0.2, 0.6)
-    clip = uniform(0.4, 2)
+    n_layers = randint(1, 4)
+    dropout = uniform(0.15, 0.65)
+    clip = uniform(0.4, 1)
 
-    command = "python train.py --cuda --dev_every 2000 --log_every 1000 --batch_size 128 " \
+    command = "python train.py --cuda --dev_every 500 --log_every 250 --batch_size 128 " \
                 "--epochs {} --lr {} --d_hidden {} --n_layers {} --dropout_prob {} --clip {} >> " \
                     "results.txt".format(epochs, learning_rate, d_hidden, n_layers, dropout, clip)
 
