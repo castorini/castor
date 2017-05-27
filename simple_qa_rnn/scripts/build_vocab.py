036f3f1d97af86e0f0c70fb2ce5f872ae0a9703f
Salman Mohammed <smohammed1993@gmail.com>
2017-05-26 22:29:47 -0400

    changed build vocab

--- simple_qa_rnn/scripts/build_vocab.py
+++ simple_qa_rnn/scripts/build_vocab.py
@@ -7,7 +7,6 @@ import torch
 def build_vocab_SQ(data_dir):
     filepaths = glob.glob(os.path.join(data_dir, 'annotated*.txt'))
     print("reading filepaths: {}".format(filepaths))
-    dst_path = os.path.join(data_dir, 'vocab.pt')
     word_vocab = set()
     relation_vocab = set()
     for filepath in filepaths:
@@ -25,13 +24,14 @@ def build_vocab_SQ(data_dir):
 
     wv_dict = {word: i for i, word in enumerate(sorted(word_vocab))}  # word to index dictionary
     relation_dict = {relation: i for i, relation in enumerate(sorted(relation_vocab))}  # relation to index dictionary
-    ret = (wv_dict, relation_dict) # tuple to write to dst_path
-    print("saving word2index and answer2index dicts to {}".format(dst_path))
-    torch.save(ret, dst_path)
+    return (wv_dict, relation_dict)
 
 
 print("WARNING: This script is dataset specific. Please change it to fit your own dataset.")
 data_dir = '../data/SimpleQuestions_v2/'
+dst_path = os.path.join(data_dir, 'vocab.pt')
 print("Building vocab for data in: {}".format(data_dir))
-build_vocab_SQ(data_dir)
+ret = build_vocab_SQ(data_dir) # tuple to write to dst_path
+print("saving word2index and answer2index dicts to {}".format(dst_path))
+torch.save(ret, dst_path)
 print("Done!")
