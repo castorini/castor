703017b36115a20f624f5ac7a95f9ab04e0b4760
Michael Tu <tuzhucheng@outlook.com>
2017-07-08 23:00:36 -0400

    MP-CNN: implement sentence modeling module

--- mp_cnn/main.py
+++ mp_cnn/main.py
@@ -3,14 +3,29 @@ import os
 
 import numpy as np
 import torch
+from torch.autograd import Variable
 
 from dataset import DatasetType, MPCNNDataset
+from model import MPCNN
 import preprocessing
 
 
-def train(train_loader, epochs):
+# logging setup
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+def train(model, train_loader, epochs):
     for batch_idx, (sentences, labels) in enumerate(train_loader):
-        pass
+        sent_a, sent_b = sentences['a'], sentences['b']
+        output = model(Variable(sent_a), Variable(sent_b))
 
 
 if __name__ == '__main__':
@@ -18,13 +33,18 @@ if __name__ == '__main__':
     parser.add_argument('--dataset_folder', help='directory containing train, dev, test sets', default=os.path.join(os.pardir, os.pardir, 'data', 'sick'))
     parser.add_argument('--word_vectors_file', help='word vectors file', default=os.path.join(os.pardir, os.pardir, 'data', 'GloVe', 'glove.840B.300d.txt'))
     parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 10)')
+    parser.add_argument('--holistic_filters', type=int, default=300, metavar='N', help='number of holistic filters')
+    parser.add_argument('--per_dim_filters', type=int, default=20, metavar='N', help='number of per-dimension filters')
     args = parser.parse_args()
 
     torch.manual_seed(1234)
     np.random.seed(1234)
 
     word_index, embedding = preprocessing.get_glove_embedding(args.word_vectors_file, args.dataset_folder)
+    logger.info('Finished loading GloVe embedding for vocab in data...')
 
     train_loader = torch.utils.data.DataLoader(MPCNNDataset(args.dataset_folder, DatasetType.TRAIN, word_index, embedding), batch_size=1)
 
-    train(train_loader, args.epochs)
+    filter_widths = [1, 2, 3]
+    model = MPCNN(300, args.holistic_filters, args.per_dim_filters, filter_widths)
+    train(model, train_loader, args.epochs)
---
+++ mp_cnn/model.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class MPCNN(nn.Module):
+
+    def __init__(self, n_word_dim, n_holistic_filters, n_per_dim_filters, filter_widths):
+        super(MPCNN, self).__init__()
+
+        self.n_word_dim = n_word_dim
+        self.n_per_dim_filters = n_per_dim_filters
+        self.filter_widths = filter_widths
+        self.holistic_conv_layers = {}
+        self.per_dim_conv_layers = {}
+
+        for ws in filter_widths:
+            self.holistic_conv_layers[ws] = nn.Sequential(
+                nn.Conv1d(n_word_dim, n_holistic_filters, ws),
+                nn.Tanh()
+            )
+
+            self.per_dim_conv_layers[ws] = nn.Sequential(
+                nn.Conv1d(n_word_dim, n_word_dim * n_per_dim_filters, ws, groups=n_word_dim),
+                nn.Tanh()
+            )
+
+    def forward(self, sent1, sent2):
+        block_a = {}
+        block_b = {}
+        for ws in self.filter_widths:
+            holistic_conv_out = self.holistic_conv_layers[ws](sent1)
+            block_a[ws] = {
+                'max': F.max_pool1d(holistic_conv_out, holistic_conv_out.size()[2]),
+                'min': F.max_pool1d(-1 * holistic_conv_out, holistic_conv_out.size()[2]),
+                'mean': F.avg_pool1d(holistic_conv_out, holistic_conv_out.size()[2])
+            }
+
+            per_dim_conv_out = self.per_dim_conv_layers[ws](sent1)
+            per_dim_conv_out = per_dim_conv_out.view(self.n_word_dim, self.n_per_dim_filters, -1)
+            block_b[ws] = {
+                'max': F.max_pool2d(per_dim_conv_out, (1, per_dim_conv_out.size()[2])),
+                'min': F.max_pool2d(-1 * per_dim_conv_out, (1, per_dim_conv_out.size()[2]))
+            }
+
+        # TODO run through conv/poolinf for sent2 as well
+        # TODO handle ws = infinity
+        # TODO implement similarity measurement layer and fully-connected layer
+        # return dummy return values for now
+        return Variable(torch.Tensor([0, 0]))
