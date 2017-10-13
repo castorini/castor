35d4cc19659d94c5f69470bbe2ed6f759ec9278f
rdsequie <rdsequie@uwaterloo.ca>
2017-10-12 21:05:14 -0400

    minor fix

--- sm_modified_cnn/utils.py
+++ sm_modified_cnn/utils.py
@@ -1,30 +1,23 @@
 from tqdm import tqdm
-import array
 import torch
-import numpy as np
 
 from gensim.models.keyedvectors import KeyedVectors
 from argparse import ArgumentParser
 
 
 def convert(fname, save_file):
-    stoi, vectors = [], array.array('d')
-
-    vocab_size, dim = None, None
     with open(fname, 'rb') as dim_file:
         vocab_size, dim = (int(x) for x in dim_file.readline().split())
 
-    W = np.memmap(fname, dtype=np.double, shape=(vocab_size, dim))
+    word_vectors = KeyedVectors.load_word2vec_format(fname, binary=True)
 
     print("Loading vectors from {}".format(fname))
     vectors = []
-    for line in tqdm(W, total=len(W)):
-        entry = line
-        vectors.extend(entry)
+    for line in tqdm(word_vectors.syn0, total=len(word_vectors.syn0)):
+        vectors.extend(line.tolist())
     vectors = torch.Tensor(vectors).view(-1, dim)
 
-    wv = KeyedVectors.load_word2vec_format(fname, binary=True)
-    stoi = {word.strip():voc.index for word, voc in wv.vocab.items()}
+    stoi = {word.strip():voc.index for word, voc in word_vectors.vocab.items()}
 
     print('saving vectors to', save_file)
     torch.save((stoi, vectors, dim), save_file)
