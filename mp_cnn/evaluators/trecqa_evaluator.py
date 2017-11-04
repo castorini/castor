5c7e8569db620a17cb1b0c03b27f78f1d75452a8
Michael Tu <tuzhucheng@outlook.com>
2017-11-04 17:57:26 -0400

    Move map and mrr computation to Castor utils

--- mp_cnn/evaluators/trecqa_evaluator.py
+++ mp_cnn/evaluators/trecqa_evaluator.py
@@ -1,32 +1,7 @@
-import os
-import subprocess
-import time
-
 import torch.nn.functional as F
 
 from mp_cnn.evaluators.evaluator import Evaluator
-
-
-def get_map_mrr(qids, predictions, labels, device):
-    qrel_fname = 'trecqa_{}_{}.qrel'.format(time.time(), device)
-    results_fname = 'trecqa_{}_{}.results'.format(time.time(), device)
-    qrel_template = '{qid} 0 {docno} {rel}\n'
-    results_template = '{qid} 0 {docno} 0 {sim} mpcnn\n'
-    with open(qrel_fname, 'w') as f1, open(results_fname, 'w') as f2:
-        docnos = range(len(qids))
-        for qid, docno, predicted, actual in zip(qids, docnos, predictions, labels):
-            f1.write(qrel_template.format(qid=qid, docno=docno, rel=actual))
-            f2.write(results_template.format(qid=qid, docno=docno, sim=predicted))
-
-    trec_out = subprocess.check_output(['../utils/trec_eval-9.0.5/trec_eval', '-m', 'map', '-m', 'recip_rank', qrel_fname, results_fname])
-    trec_out_lines = str(trec_out, 'utf-8').split('\n')
-    mean_average_precision = float(trec_out_lines[0].split('\t')[-1])
-    mean_reciprocal_rank = float(trec_out_lines[1].split('\t')[-1])
-
-    os.remove(qrel_fname)
-    os.remove(results_fname)
-
-    return mean_average_precision, mean_reciprocal_rank
+from utils.relevancy_metrics import get_map_mrr
 
 
 class TRECQAEvaluator(Evaluator):
