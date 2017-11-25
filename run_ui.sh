f28214438a188433a57f96257ab3f40ddadb0cb5
MeowFei <m263liu@edu.uwaterloo.ca>
2017-11-25 13:24:50 -0500

    added documentation

--- run_ui.sh
+++ run_ui.sh
@@ -1,18 +1,11 @@
 #!/usr/bin/env bash
 
-#echo "Start PyseriniEntryPoint..."
-#sh target/appassembler/bin/PyseriniEntryPoint &
-#PID_1=$!
-
 echo "Start the Flask server..."
-#python3 src/main/python/api.py &
-#PID_2=$!
 python3 anserini_dependency/api.py --model idf &
 PID_2=$!
 
 
 echo "Start the JavaScript UI..."
-#pushd src/main/js
 pushd anserini_dependency/js
 npm start &
 PID_3=$!
@@ -22,7 +15,6 @@ popd
 function clean_up {
     kill $PID_3
     kill $PID_2
-    #kill $PID_1
     exit
 }
 
