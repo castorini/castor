1eac277cb8046842ad4fb3abccafeffd49fb17f7
Achyudh Ram <achyudhk@gmail.com>
2018-10-26 18:00:26 -0400

    Remove batch_size condition in ReutersTrainer

--- common/trainers/reuters_trainer.py
+++ common/trainers/reuters_trainer.py
@@ -19,7 +19,6 @@ class ReutersTrainer(Trainer):
         self.iterations = 0
         self.iters_not_improved = 0
         self.start = None
-        self.batch_size = trainer_config['batch_size']
         self.log_template = ' '.join(
             '{:>6.0f},{:>5.0f},{:>9.0f},{:>5.0f}/{:<5.0f} {:>7.0f}%,{:>8.6f},{},{:12.4f},{}'.split(','))
         self.dev_log_template = ' '.join('{:>6.0f},{:>5.0f},{:>9.0f},{:>5.0f}/{:<5.0f} {:>7.0f}%,{:>8.6f},{:8.6f},{:12.4f},{:12.4f}'.split(','))
@@ -29,53 +28,52 @@ class ReutersTrainer(Trainer):
         self.train_loader.init_epoch()
         n_correct, n_total = 0, 0
         for batch_idx, batch in enumerate(self.train_loader):
-            if batch.batch_size == self.batch_size:
-                self.iterations += 1
-                self.model.train()
-                self.optimizer.zero_grad()
-                scores = self.model(batch.text[0], lengths=batch.text[1])
-                # Using binary accuracy
-                for tensor1, tensor2 in zip(F.sigmoid(scores).round().long(), batch.label):
-                    if np.array_equal(tensor1, tensor2):
-                        n_correct += 1
-                n_total += batch.batch_size
-                train_acc = 100. * n_correct / n_total
-                loss = F.binary_cross_entropy_with_logits(scores, batch.label.float())
-                loss.backward()
+            self.iterations += 1
+            self.model.train()
+            self.optimizer.zero_grad()
+            scores = self.model(batch.text[0], lengths=batch.text[1])
+            # Using binary accuracy
+            for tensor1, tensor2 in zip(F.sigmoid(scores).round().long(), batch.label):
+                if np.array_equal(tensor1, tensor2):
+                    n_correct += 1
+            n_total += batch.batch_size
+            train_acc = 100. * n_correct / n_total
+            loss = F.binary_cross_entropy_with_logits(scores, batch.label.float())
+            loss.backward()
 
-                self.optimizer.step()
+            self.optimizer.step()
 
-                # Evaluate performance on validation set
-                if self.iterations % self.dev_log_interval == 1:
-                    dev_acc, dev_loss = self.dev_evaluator.get_scores()[0]
-                    niter = epoch * len(self.train_loader) + batch_idx
-                    self.writer.add_scalar('Train/Loss', loss.data[0], niter)
-                    self.writer.add_scalar('Dev/Loss', dev_loss, niter)
-                    self.writer.add_scalar('Train/Accuracy', train_acc, niter)
-                    self.writer.add_scalar('Dev/Accuracy', dev_acc, niter)
-                    print(self.dev_log_template.format(time.time() - self.start,
-                          epoch, self.iterations, 1 + batch_idx, len(self.train_loader),
-                          100. * (1 + batch_idx) / len(self.train_loader), loss.item(),
-                          dev_loss, train_acc, dev_acc))
+            # Evaluate performance on validation set
+            if self.iterations % self.dev_log_interval == 1:
+                dev_acc, dev_loss = self.dev_evaluator.get_scores()[0]
+                niter = epoch * len(self.train_loader) + batch_idx
+                self.writer.add_scalar('Train/Loss', loss.data[0], niter)
+                self.writer.add_scalar('Dev/Loss', dev_loss, niter)
+                self.writer.add_scalar('Train/Accuracy', train_acc, niter)
+                self.writer.add_scalar('Dev/Accuracy', dev_acc, niter)
+                print(self.dev_log_template.format(time.time() - self.start,
+                      epoch, self.iterations, 1 + batch_idx, len(self.train_loader),
+                      100. * (1 + batch_idx) / len(self.train_loader), loss.item(),
+                      dev_loss, train_acc, dev_acc))
 
-                    # Update validation results
-                    if dev_acc > self.best_dev_acc:
-                        self.iters_not_improved = 0
-                        self.best_dev_acc = dev_acc
-                        snapshot_path = os.path.join(self.model_outfile, self.train_loader.dataset.NAME, self.model.mode + '_best_model.pt')
-                        torch.save(self.model, snapshot_path)
-                    else:
-                        self.iters_not_improved += 1
-                        if self.iters_not_improved >= self.patience:
-                            self.early_stop = True
-                            break
+                # Update validation results
+                if dev_acc > self.best_dev_acc:
+                    self.iters_not_improved = 0
+                    self.best_dev_acc = dev_acc
+                    snapshot_path = os.path.join(self.model_outfile, self.train_loader.dataset.NAME, self.model.mode + '_best_model.pt')
+                    torch.save(self.model, snapshot_path)
+                else:
+                    self.iters_not_improved += 1
+                    if self.iters_not_improved >= self.patience:
+                        self.early_stop = True
+                        break
 
-                if self.iterations % self.log_interval == 1:
-                    # print progress message
-                    print(self.log_template.format(time.time() - self.start,
-                                              epoch, self.iterations, 1 + batch_idx, len(self.train_loader),
-                                              100. * (1 + batch_idx) / len(self.train_loader), loss.item(), ' ' * 8,
-                                              train_acc, ' ' * 12))
+            if self.iterations % self.log_interval == 1:
+                # print progress message
+                print(self.log_template.format(time.time() - self.start,
+                                          epoch, self.iterations, 1 + batch_idx, len(self.train_loader),
+                                          100. * (1 + batch_idx) / len(self.train_loader), loss.item(), ' ' * 8,
+                                          train_acc, ' ' * 12))
 
     def train(self, epochs):
         self.start = time.time()
