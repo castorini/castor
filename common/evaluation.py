9d63bb8894ed2f53e84c7771635af083b7764fcd
Achyudh Ram <achyudhk@gmail.com>
2019-04-07 19:58:06 -0400

    Remove ReutersEvaluator

--- common/evaluation.py
+++ common/evaluation.py
@@ -4,7 +4,6 @@ from .evaluators.sst_evaluator import SSTEvaluator
 from .evaluators.trecqa_evaluator import TRECQAEvaluator
 from .evaluators.wikiqa_evaluator import WikiQAEvaluator
 from .evaluators.pit2015_evaluator import PIT2015Evaluator
-from .evaluators.reuters_evaluator import ReutersEvaluator
 from .evaluators.snli_evaluator import SNLIEvaluator
 from .evaluators.sts2014_evaluator import STS2014Evaluator
 from .evaluators.quora_evaluator import QuoraEvaluator
@@ -25,10 +24,6 @@ class EvaluatorFactory(object):
         'wikiqa': WikiQAEvaluator,
         'pit2015': PIT2015Evaluator,
         'twitterurl': PIT2015Evaluator,
-        'Reuters': ReutersEvaluator,
-        'AAPD': ReutersEvaluator,
-        'IMDB': ReutersEvaluator,
-        'Yelp2014': ReutersEvaluator,
         'SNLI': SNLIEvaluator,
         'sts2014': STS2014Evaluator,
         'Quora': QuoraEvaluator
